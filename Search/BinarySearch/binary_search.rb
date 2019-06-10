53e2ac585331d5292f0bd68919d24edfd815ce67
Katsuki <Katsuki_Sato0130@icloud.com>
2019-06-10 20:57:59 +0900

    Add Ruby Binary Search

--- Search/BinarySearch/binary_search.rb
+++ Search/BinarySearch/binary_search.rb
@@ -1,8 +1,18 @@
 def binary_search(sorted_list, target)
-  middle = sorted_list[sorted_list.length/2]
+  s = 0
+  m = sorted_list.size
 
-  if middle == target
-    true
-  elsif middle < target
+  while s < m
+    middle = (s + m) / 2
+    if target == sorted_list[middle]
+      return middle
+    elsif target > sorted_list[middle]
+      s = middle + 1
+    elsif target < sorted_list[middle]
+      m = middle - 1
+    end
   end
 end
+
+list = [1,2,3,4,5,6,7,8,9,10]
+p binary_search(list, 7) #=> 6
