d3ad82ad99a97c586cce0cba4c3d56d68f72092c
Katsuki <Katsuki_Sato0130@icloud.com>
2020-08-31 15:02:32 +0900

    Update BinarySearch in Ruby

--- Search/BinarySearch/Ruby/binary_search.rb
+++ Search/BinarySearch/Ruby/binary_search.rb
@@ -22,6 +22,24 @@ def iterative_binary_search(sorted_list, target)
   raise NoSuchValueError
 end
 
+# Returns the index of the value if the value exists and raises an error if the value dose not exist in the given array.
+def recursive_binary_search(array, target, low, high)
+  mid = (low + high) / 2
+  # Raise error if the value of high is bigger that the one of low.
+  raise NoSuchValueError if high < low
+    # If the value in the middle is bigger that the target,
+    # pass the value of `low` to the `low` parameter again and pass `mid - 1` to the `high` parameter.
+    return recursive_binary_search(array, target, low, mid - 1) if (array[mid] > target)
+
+    # If the value in the middle is smaller that the target,
+    # pass the value of `high` to the `high` parameter again and pass `mid + 1` to the `low` parameter.
+  return recursive_binary_search(array, target, mid + 1, high) if (array[mid] < target)
+
+  return mid if (array[mid] == target)
+end
+
+
+# Returns true if the value exists and raises an error if the value dose not exist in the given array.
 def recursive_binary_search2(array, target)
   low = 0
   high = array.length - 1
@@ -42,15 +60,3 @@ def recursive_binary_search2(array, target)
     srecursive_binary_search(array[low, middle - 1], target)
   end
 end
-
-
-def recursive_binary_search(array, target, low, high)
-  mid = (low + high) / 2
-  p mid
-  raise NoSuchValueError if high < low
-  return recursive_binary_search(array, target, low, mid - 1) if (array[mid] > target)
-  return recursive_binary_search(array, target, mid + 1, high) if (array[mid] < target)
-  return mid if (array[mid] == target)
-end
-
-recursive_binary_search([1,2,3,4,5,6,7,8,9], 6, 0, 8)
