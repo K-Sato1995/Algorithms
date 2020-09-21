172160548c697b8d57733c5a8c462daefa3f0546
Katsuki <Katsuki_Sato0130@icloud.com>
2020-09-21 12:14:23 +0900

    Update merge_sort in Ruby

--- Sort/MergeSort/Ruby/merge_sort.rb
+++ Sort/MergeSort/Ruby/merge_sort.rb
@@ -1,1 +1,28 @@
-# https://www.google.com/search?q=ruby+mergeSort&oq=ruby+mergeSort&aqs=chrome..69i57j0l7.2879j0j7&sourceid=chrome&ie=UTF-8
+# https://medium.com/@allegranzia/merge-sort-in-ruby-102b750af287
+
+def merge_sort(unsorted_array) 
+  # if array only has one element or fewer there is nothing to do
+  return unsorted_array if unsorted_array.length <=1 
+
+  # dividing and then merge-sorting the halves
+  mid = unsorted_array.length/2
+  first_half = merge_sort(unsorted_array.slice(0...mid))
+  second_half = merge_sort(unsorted_array.slice(mid...unsorted_array.length))
+  merge(first_half, second_half)
+end
+
+# Merge given two arrays into one array in order.
+def merge(left_array, right_array)
+  sorted_array = []
+  # If either array is empty we don't need to compare them
+  while !left_array.empty? && !right_array.empty? do
+      # we are shifting out the compared/used values so we don't repeat
+      if left_array[0] < right_array[0]
+          sorted_array.push(left_array.shift)
+      else   
+          sorted_array.push(right_array.shift)
+      end
+  end 
+  #concat appends elements of another array to an array
+  return sorted_array.concat(left_array).concat(right_array)
+end
--- Sort/MergeSort/Ruby/merge_sort_test.rb
+++ Sort/MergeSort/Ruby/merge_sort_test.rb
@@ -1,15 +1,14 @@
 require 'minitest/autorun'
 require_relative 'merge_sort'
 
-# test for the merge sort
 describe "merge_sort" do
   TEST_DATA = [8, 4, 3, 1, 2, 6, 5, 7];
 
-  it "returns the position of the given value in the given list" do
+  it "returns the sorted list" do
     expect(merge_sort(TEST_DATA)).must_equal([1, 2, 3, 4, 5, 6, 7, 8])
   end
 
-  it "raises NoSuchValueError when the given value dose not exist in the list" do
-    expect{ merge_sort([]) }.must_raise(ArrayIsEmptyError)
+  it "raises ArrayIsEmptyError when the given array is empty" do
+    expect{ merge_sort([]) }.must_equal([])
   end
 end
