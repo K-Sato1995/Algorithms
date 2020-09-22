e7e5d5f95bd71402cd98660094460c2f0bb437e7
Katsuki <Katsuki_Sato0130@icloud.com>
2020-09-22 10:48:39 +0900

    Add comment

--- Sort/MergeSort/Ruby/merge_sort_test.rb
+++ Sort/MergeSort/Ruby/merge_sort_test.rb
@@ -8,6 +8,7 @@ describe "merge_sort" do
     expect(merge_sort(TEST_DATA)).must_equal([1, 2, 3, 4, 5, 6, 7, 8])
   end
 
+  # TODO: Fix test
   it "raises ArrayIsEmptyError when the given array is empty" do
     expect{ merge_sort([]) }.must_equal([])
   end
