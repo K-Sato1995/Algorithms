5f26baaf4466c3e598c2ee973bd9f640e605b9f8
Katsuki Sato <katsuki_sato0130@icloud.com>
2020-12-16 08:16:54 +0900

    Add roman to Int

---
+++ Practice/RomantoInteger/romanToInt.ts
@@ -0,0 +1,28 @@
+// type romanNums = "I" | "V" | "X" | "L" | "C" | "D" | "M"
+
+const romanIntTable: Record<string, number> = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  };
+  
+function romanToInt(s: string): number {
+    // 文字列を配列にしてループ
+    const romanArr = s.split('')
+    let result = 0
+
+    for(let i = 0; i < romanArr.length; i++){
+        if(romanIntTable[romanArr[i]] < romanIntTable[romanArr[i + 1]] ){
+            result += romanIntTable[romanArr[i + 1]] - romanIntTable[romanArr[i]]
+        } else {
+            result += romanIntTable[romanArr[i]]
+        }
+    }
+
+    return result
+};
+
