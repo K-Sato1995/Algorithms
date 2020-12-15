7d33537b899f23c751acd8336c43fc1e8d08c09b
Katsuki Sato <katsuki_sato0130@icloud.com>
2020-12-16 08:25:37 +0900

    Update test

--- Practice/RomantoInteger/romanToInt.test.ts
+++ Practice/RomantoInteger/romanToInt.test.ts
@@ -5,4 +5,16 @@ describe("RomanToInt", () => {
     const data = "III";
     expect(romanToInt(data)).toEqual(3);
   });
+  it("converts numerals to numbers", () => {
+    const data = "IV";
+    expect(romanToInt(data)).toEqual(4);
+  });
+  it("converts numerals to numbers", () => {
+    const data = "IX";
+    expect(romanToInt(data)).toEqual(9);
+  });
+  it("converts numerals to numbers", () => {
+    const data = "LVIII";
+    expect(romanToInt(data)).toEqual(58);
+  });
 });
