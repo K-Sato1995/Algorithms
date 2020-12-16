bf2ca52336f367e142532b888794bcd518751025
Katsuki Sato <katsuki_sato0130@icloud.com>
2020-12-16 20:21:27 +0900

    Add palindromeNum'

---
+++ Practice/PalindromeNumber/palindromeNum.test.ts
@@ -0,0 +1,20 @@
+import { isPalindrome } from "./palindromeNum";
+
+describe("PalindromeNumber", () => {
+  it("checks if the given number is palindrome", () => {
+    const data = 121;
+    expect(isPalindrome(data)).toBeTruthy();
+  });
+  it("checks if the given number is palindrome", () => {
+    const data = -121;
+    expect(isPalindrome(data)).toBeFalsy();
+  });
+  it("checks if the given number is palindrome", () => {
+    const data = 10;
+    expect(isPalindrome(data)).toBeFalsy();
+  });
+  it("checks if the given number is palindrome", () => {
+    const data = -101;
+    expect(isPalindrome(data)).toBeFalsy();
+  });
+});
---
+++ Practice/PalindromeNumber/palindromeNum.ts
@@ -0,0 +1,8 @@
+function isPalindrome(num: number): boolean {
+    if (num < 0) return false      
+
+
+    return true
+};
+
+export { isPalindrome }
