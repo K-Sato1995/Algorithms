660f17d1caa6779e4082c450a18535774908b0bd
Katsuki Sato <katsuki_sato0130@icloud.com>
2020-12-19 14:47:46 +0900

    Add validate same letter

--- CTCI/Chapter1/question1.test.ts
+++ CTCI/Chapter1/question1.test.ts
@@ -1,4 +1,11 @@
+import { validateSameLetter } from "./question1"
 
-// describe('', () => {
-//     expect(isPalindrome(data)).toBeTruthy();  
-// })
+describe('validateSameLetter', () => {
+    it('returns true if the string consists of unique letters', () => {
+        expect(validateSameLetter("abcdefghijklmn")).toBeTruthy();
+    })
+
+    it('returns false if the string dose not consist of unique letters', () => {
+        expect(validateSameLetter("abcdefgb")).toBeFalsy();
+    })
+})
--- CTCI/Chapter1/question1.ts
+++ CTCI/Chapter1/question1.ts
@@ -1,3 +1,15 @@
-const allUniqueLetters = (s: string): boolean => {  
-    return false
+// Convert the string to array and brute force = O(N * 2)
+const validateSameLetter = (s: string): boolean => {  
+    const stringArr = s.split('')
+    const arrLength = stringArr.length
+
+    for(let i = 0; i < arrLength; i++) {
+        for(let j = i + 1; j < arrLength; j++) {
+            if(stringArr[i] === stringArr[j]) return false
+        }
+    }
+    return true
 }
+
+
+export { validateSameLetter }
