f2985d1a70994fd32adad315c0e13e0d0c5e2e2c
Katsuki Sato <katsuki_sato0130@icloud.com>
2020-12-22 20:52:22 +0900

    Add more code for linked list

--- DataStructures/LinkedList/index.ts
+++ DataStructures/LinkedList/index.ts
@@ -1,6 +1,7 @@
 // 配列とは異なり、連結リストには特定のIndexでアクセスはできない
 // リスト内のK番目の要素を見つけたい場合はheadからK個分の要素を走査する必要がある
 // O(n)
+// https://codeburst.io/linked-lists-in-javascript-es6-code-part-1-6dd349c3dcc3#:~:text=To%20remove%20the%20last%20node,the%20last%20node%20to%20null.
 class ListNode {
     value: number
     next?: ListNode
@@ -44,15 +45,20 @@ const push = (head: ListNode, value: number): ListNode => {
 }
 
 // Delte a node at the given position
-const deleteNode = (head: ListNode, position: number) => {
+const deleteNode = (head: ListNode | undefined, key: number) => {
+    let currentNode = head
 
-    let currentNode: ListNode = head
+    if (currentNode === null) return
 
-    while(currentNode.next !== undefined){
-
+    if(currentNode?.value === key) {
+        currentNode = currentNode.next
     }
-
-    return head
+    
+    while(currentNode?.next !== undefined){
+        if(currentNode.next.value === key) {
+            currentNode.next = currentNode.next.next
+        }
+    }
 }
 // const reverseList = (head: ListNode) => {
 //     let currentNode: ListNode = head
@@ -78,4 +84,4 @@ node2.next = node3
 node3.next = node4
 
 // console.log(countNode(node1)) //=> 4
-export { ListNode, append, push, countNode }
+export { ListNode, append, push, countNode, deleteNode }
