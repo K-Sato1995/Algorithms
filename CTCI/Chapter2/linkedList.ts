4e30912001de54a5442d436358fe1c64121de0ea
Katsuki Sato <katsuki_sato0130@icloud.com>
2020-12-28 16:42:40 +0900

    Add tree

--- CTCI/Chapter2/linkedList.ts
+++ CTCI/Chapter2/linkedList.ts
@@ -1,3 +1,4 @@
+// https://www.geeksforgeeks.org/implementation-linkedlist-javascript/
 class ListNode { 
     value: number
     next: ListNode | null
---
+++ DataStructures/Tree/index.ts
@@ -0,0 +1,51 @@
+// https://adrianmejia.com/data-structures-for-beginners-trees-binary-search-tree-tutorial/
+
+class TreeNode {
+    value: number | null
+    descendents: TreeNode[]
+
+    constructor(value: number) {
+        this.value = value
+        this.descendents = []
+    }
+}
+
+const node1 = new TreeNode(1)
+const node2 = new TreeNode(2)
+const node3 = new TreeNode(3)
+const node4 = new TreeNode(4)
+const node5 = new TreeNode(5)
+
+node1.descendents.push(node2)
+node2.descendents.push(node3, node4, node5)
+// This is what it looks like now
+//      node1 
+//        ↓
+//      node2
+//  ↙︎　　　↓     ↘︎
+// node3 node4 node5
+// console.log(node1)
+// =>
+// TreeNode: {
+//     "value": 1,
+//     "descendents": [
+//       {
+//         "value": 2,
+//         "descendents": [
+//           {
+//             "value": 3,
+//             "descendents": []
+//           },
+//           {
+//             "value": 4,
+//             "descendents": []
+//           },
+//           {
+//             "value": 5,
+//             "descendents": []
+//           }
+//         ]
+//       }
+//     ]
+//   } 
+
