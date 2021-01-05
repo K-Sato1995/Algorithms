f901569e1dab282098f8ce83fb1f988af6988a17
Katsuki Sato <katsuki_sato0130@icloud.com>
2021-01-05 20:26:29 +0900

    Rewrite bts

--- DataStructures/BinarySearchTree/index.ts
+++ DataStructures/BinarySearchTree/index.ts
@@ -1,57 +1,111 @@
-const LEFT = 0 
-const RIGHT = 1
-
-class BinarySearchTreeNode {
-    value: number 
-    // BSTなので、decendantsには最大2個までしか入らない
-    descendents: BinarySearchTreeNode[]
-    parent: BinarySearchTreeNode | null
+class BSTNode {
+    value: number
+    right: BSTNode | null
+    left: BSTNode | null
 
     constructor(value: number) {
         this.value = value
-        this.descendents = []
-        this.parent = null
-    }
-
-    get left() {
-        return this.descendents[LEFT]
-    }
-
-    set left(node: BinarySearchTreeNode) {
-        this.descendents[LEFT] = node
-        if(node){
-            node.parent = this
-        }
-    }
-
-    get right() {
-        return this.descendents[RIGHT]
-    }
-
-    set right(node: BinarySearchTreeNode) {
-        this.descendents[RIGHT] = node
-        if(node) {
-            node.parent = this
-        }
+        this.right = null
+        this.left = null
     }
 }
 
 class BinarySearchTree {
-    root: BinarySearchTreeNode | null
-    size: number
+    root: BSTNode | null
 
     constructor() {
         this.root = null
-        this.size = 0
     }
-    // BSTに要素を追加する方法
-    // (1) If a tree is empty, the first node becomes the root, and you are done.
-    // (2) Compare root/parent’s value if it’s higher go right, if it’s lower go left. If it’s the same, then the value already exists so that you can increase the duplicate count (multiplicity).
-    // (3) Repeat #2 until we found an empty slot to insert the new node.
+
     add(value: number) {
-        const newNode = new BinarySearchTreeNode(value)
-        if(this.root) {
-            // const { found, }
+        const newNode = new BSTNode(value)
+
+        // Set the new node as the root if there is no tree yet
+        if(!this.root) {
+            this.root = newNode
+            return this
+        } else {
+            let current = this.root
+            while(true) {
+                // if the new value is less than the current node's value, go left
+                if(value < current.value) {
+                    if(!current.left) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left
+                    }
+                // if the new value is greater than the current node's value, go right
+                } else if(value > current.value) {
+                    if(!current.right) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                //if the new value is equal to the current one, just ignore
+                } else {
+                    return this
+                }
+            }
         }
     }
- }
+
+    find(value: number) {
+        // if tree is not there yet, just returns false
+        if(!this.root) return false
+
+        let current: BSTNode | null = this.root
+        let isFound = false
+
+        while(current && !isFound) {
+            // if the value is smaller thatn than the current node's value, go left
+            if(value < current.value) {
+                current = current.left
+            //if the value is greater than the current node's, go right
+            } else if (value > current.value) {
+                current = current.right
+            // if values are equal, found it!
+            } else {
+                isFound = true
+            }
+        }
+
+        return isFound
+    }
+}
+
+
+let tree = new BinarySearchTree();
+tree.add(10);
+tree.add(4);
+tree.add(4);
+tree.add(12);
+tree.add(2);
+console.log(tree);
+
+/*
+ BinarySearchTree: {
+  "root": {
+    "value": 10,
+    "right": {
+      "value": 12,
+      "right": null,
+      "left": null
+    },
+    "left": {
+      "value": 4,
+      "right": null,
+      "left": {
+        "value": 2,
+        "right": null,
+        "left": null
+      }
+    }
+  }
+}
+*/
+
+console.log(tree.find(4)) //=> true
+console.log(tree.find(12)) //=> true
+console.log(tree.find(3)) //=> false
