27a34876813446d1d744c1473049cf473bcacc0e
Katsuki Sato <katsuki_sato0130@icloud.com>
2021-01-11 15:27:53 +0900

    Add binary tree

---
+++ DataStructures/Tree/BinaryTree/index.ts
@@ -0,0 +1,11 @@
+class BinaryTreeNode {
+  value: number
+  right: BinaryTreeNode | null
+  left: BinaryTreeNode | null
+
+  constructor(value: number) {
+    this.value = value
+    this.right = null
+    this.left = null
+  }
+}
