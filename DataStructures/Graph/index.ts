bc4ec092f1d9ac191bff6da7795d0354b0129475
Katsuki Sato <katsuki_sato0130@icloud.com>
2021-01-13 19:56:27 +0900

    New implementation for Graph

--- DataStructures/Graph/index.ts
+++ DataStructures/Graph/index.ts
@@ -5,57 +5,94 @@ https://adrianmejia.com/data-structures-for-beginners-graphs-time-complexity-tut
 */
 
 // 隣接リスト(Adjaceny List)によるグラフの実装方法
-class GraphNode {
-  value: number
-  adjacents: GraphNode[]
-
-  constructor(value: number) {
-    this.value = value
-    this.adjacents = []
-  }
-
-  addAdjacent(node: GraphNode) {
-    this.adjacents.push(node)
-  }
-
-  removeAdjacent(node: GraphNode) {
-    const index = this.adjacents.indexOf(node)
-    if (index > -1) {
-      this.adjacents.splice(index, 1)
-      return node
-    }
-  }
-
-  getAdjacents() {
-    return this.adjacents
-  }
+/*
+{
+    1: [2, 4],
+    2: [1, 3]
 }
-
-type GraphDirection = 'Directed' | 'Undirected'
+*/
 
 class Graph {
-  nodes: Map<GraphNode, GraphNode[]>
-  edgeDirection: GraphDirection
+  nodes: { [key: number]: number[] }
 
-  constructor(edgeDirection: GraphDirection) {
-    this.nodes = new Map()
-    this.edgeDirection = edgeDirection
+  constructor() {
+    this.nodes = {}
   }
 
-  //   addEdge(source, destination: GraphNode) {}
-
-  addVertex(value: number) {
-    if (this.nodes.has(value)) {
-      return this.nodes.get(value)
-    } else {
-      const vertex = new GraphNode(value)
-      this.nodes.set(value, vertex)
-      return vertex
+  // Add new Node
+  addNode(node: number) {
+    // nodeがHashMapに存在しない場合はHashMapに空の配列と共に追加
+    if (!this.nodes[node]) {
+      this.nodes[node] = []
     }
+  }
+
+  addEdge(source: number, destination: number) {
+    // sourceがnodesに存在しない場合は新しいnodeとして追加
+    if (!this.nodes[source]) {
+      this.addNode(source)
+    }
+
+    // destinationがnodesに存在しない場合は新しいnodeとして追加
+    if (!this.nodes[destination]) {
+      this.addNode(destination)
+    }
+
+    /*
+        sourceとdesitinationが既に存在する場合、2つを繋ぐedgeを追加するため、
+        両方にそれぞれを付け加える
+    */
+    this.nodes[source].push(destination)
+    this.nodes[destination].push(source)
+  }
+
+  removeEdge(source: number, destination: number) {
+    /*
+        Sourceのadjacentsにdestinationを抜いたものをセットする
+     */
+    this.nodes[source] = this.nodes[source].filter(
+      (node: number) => node !== destination,
+    )
+    this.nodes[destination] = this.nodes[destination].filter(
+      (node: number) => node !== source,
+    )
+  }
+
+  removeNode(node: number) {
+    /*
+      取り除きたいnodeと他のnodeとの結びつきを消す
+    */
+    while (this.nodes[node]) {
+      const adjacentNode = this.nodes[node].pop()
+      if (adjacentNode) this.removeEdge(node, adjacentNode)
+    }
+    /*
+      取り除きたいnodeを削除する
+    */
+    delete this.nodes[node]
   }
 }
 
-const graph = new Graph('Directed')
+const graph = new Graph()
+
+graph.addNode(1)
+graph.addNode(2)
+graph.addNode(3)
+
+graph.addEdge(1, 2)
+graph.addEdge(2, 3)
+
+console.log(graph)
+
+/*
+Graph { 
+    nodes: { 
+        '1': [ 2 ], 
+        '2': [ 1, 3 ], 
+        '3': [ 2 ] } 
+    }
+}
+*/
 
 /*
 Adjacency matrix graph ts implementation
