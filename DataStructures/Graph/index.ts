6dc858de790498153d61389b20af03b598b96f07
Katsuki Sato <katsuki_sato0130@icloud.com>
2021-01-13 18:59:41 +0900

    Add graph

--- DataStructures/Graph/index.ts
+++ DataStructures/Graph/index.ts
@@ -1,9 +1,7 @@
-// https://adrianmejia.com/data-structures-for-beginners-graphs-time-complexity-tutorial/#Adjacency-List-Graph-OO-Implementation
-
 /*
 READ THIS AND IMPLEMENT THE GRAPH BELOW
 
-https://adrianmejia.com/data-structures-for-beginners-graphs-time-complexity-tutorial/#Adjacency-List-Graph-OO-Implementation
+https://adrianmejia.com/data-structures-for-beginners-graphs-time-complexity-tutorial
 */
 
 // 隣接リスト(Adjaceny List)によるグラフの実装方法
@@ -20,6 +18,14 @@ class GraphNode {
     this.adjacents.push(node)
   }
 
+  removeAdjacent(node: GraphNode) {
+    const index = this.adjacents.indexOf(node)
+    if (index > -1) {
+      this.adjacents.splice(index, 1)
+      return node
+    }
+  }
+
   getAdjacents() {
     return this.adjacents
   }
@@ -28,11 +34,30 @@ class GraphNode {
 type GraphDirection = 'Directed' | 'Undirected'
 
 class Graph {
-  nodes: Map<number, GraphNode[]>
+  nodes: Map<GraphNode, GraphNode[]>
   edgeDirection: GraphDirection
 
   constructor(edgeDirection: GraphDirection) {
     this.nodes = new Map()
     this.edgeDirection = edgeDirection
   }
+
+  //   addEdge(source, destination: GraphNode) {}
+
+  addVertex(value: number) {
+    if (this.nodes.has(value)) {
+      return this.nodes.get(value)
+    } else {
+      const vertex = new GraphNode(value)
+      this.nodes.set(value, vertex)
+      return vertex
+    }
+  }
 }
+
+const graph = new Graph('Directed')
+
+/*
+Adjacency matrix graph ts implementation
+https://github.com/CraigHarley/SimpleGraphJS/blob/master/Graph.ts
+*/
