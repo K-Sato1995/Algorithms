f86d7ef5e3346e0715500dae79271138074e43c3
Katsuki Sato <katsuki_sato0130@icloud.com>
2021-02-09 21:27:49 +0900

    Update some code

--- Practice/MergeTwoSortedLists/mergeTwoSortedLists.ts
+++ Practice/MergeTwoSortedLists/mergeTwoSortedLists.ts
@@ -1,5 +1,4 @@
-function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    if(l1.length == 0 && l2.length == 0) return []
-
-    for(let i = 0; )
-};
+function mergeTwoLists(
+  l1: ListNode | null,
+  l2: ListNode | null,
+): ListNode | null {}
