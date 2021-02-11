16ad7737c9acff5c8cd8af3b60497fd1a93bb0ba
Katsuki Sato <katsuki_sato0130@icloud.com>
2021-02-11 18:39:06 +0900

    Update isPalindrome

--- Practice/PalindromeNumber/palindromeNum.ts
+++ Practice/PalindromeNumber/palindromeNum.ts
@@ -1,6 +1,14 @@
-function isPalindrome(num: number): boolean {
-  if (num < 0) return false
+function isPalindrome(x: number): boolean {
+  const arr = x.toString().split('')
+
+  for (let i = 0; i < arr.length; i++) {
+    const tmp = arr[i]
+    const popedVal = arr.pop()
+
+    if (tmp !== popedVal) {
+      return false
+    }
+  }
+
   return true
 }
-
-export { isPalindrome }
