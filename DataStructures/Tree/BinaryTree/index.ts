1ddbe8da0665ac7661fdf74969fe65300629d469
Katsuki Sato <katsuki_sato0130@icloud.com>
2021-02-13 17:02:11 +0900

    Add a method that finds the height of a binary tree

--- DataStructures/Tree/BinaryTree/index.ts
+++ DataStructures/Tree/BinaryTree/index.ts
@@ -10,3 +10,16 @@ class BinaryTreeNode {
     this.left = null
   }
 }
+
+const findHeight = (node: TreeNode | null) => {
+  if (node == null) return 0
+
+  const lefth = findHeight(node.left)
+  const righth = findHeight(node.right)
+
+  if (lefth > righth) {
+    return lefth
+  } else {
+    return righth
+  }
+}
